1ad7448218abf0e8ec819f01a2e8509cedaaa97e
K-Irungu <110017552+K-Irungu@users.noreply.github.com>
2022-12-02 16:41:18 +0300

    commit tree 213bb16065f6826e49b1e8a1bcf70962d7d91550

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,25 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # Sets page_views counter to an initial value of 0
+    session[:page_views] ||= 0
+    session[:page_views] = session[:page_views].to_i + 1
+
+    # Checks if page_view limit has been reached
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article, status: :ok
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
+
   end
 
   private
